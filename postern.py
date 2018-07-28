8eee3a465d34e98f7785e40dca2d55f769bb2a1c
Steven Danneman <sdanneman@securityinnovation.com>
2018-07-28 15:18:04 -0700

    Add first simple argument parsing.

--- postern.py
+++ postern.py
@@ -1,11 +1,75 @@
 #!/usr/bin/env python3
 
-"""
-Script to...
-"""
+'''
+Fingerprint an OFX server.
+'''
+
+# Options Parsing
+# ofx-postern <url> [fid] [org]
+#             -c clear cache
+#
+# ofx-postern
+# Enter name of financial institution
+# > Cit
+# 1) Citi Bank
+# 2) Citi Bank Financial
+# Select a financial institution:
+# > 2
+
+# Output
+# 1) Connection Methods
+#    * DirectConnect (OFX)
+#    * Express Web Connect (Intuit API)
+#    * Web Connect (File download)
+# 2) Connection Info
+#    * URL
+#    * SSL Certificate expiration date
+# 3) Capabilities
+#    * Checking
+#    * Savings
+#    * Bill Pay
+#    * etc...
+# 4) Fingerprint
+#    * Service Provider?
+#    * HTTP Server
+#    * Application Framework
+#    * OFX Server
+#      - Company
+#      - Product
+#      - Version
+# 5) Tests
+#    * Information Disclosure
+#    * Zombie Server
+#    * Null values returned
+#    * Long lived session keys
+#    * XSS
+#    * TLS
+
+# Download FI list
+# - cache it
+# - create .postern in homedir
+
+# Search
+# - read FI data file
+# - pull capabilities from FI file
+
+# Send Requests
+# - cache the results
+# - determine TLS issues
+
+# Capabilities
+# - pull from PROFILE request
+# - else pull from fidata
+
+# Fingerprint
+# - review responses
+# - print results
+
+# Test
+# - review responses
+# - print results
 
 import argparse
-import logging
 import os
 import sys
 
@@ -13,15 +77,15 @@ import sys
 # Defines
 #
 
+PROGRAM_DESCRIPTION = 'Fingerprint an OFX server.'
+PROGRAM_NAME = 'ofx-postern'
+DATA_DIR = "{}/.{}".format(os.environ['HOME'], PROGRAM_NAME)
+
 #
 # Globals
 #
 
-PROGRAM_NAME = 'postern'
-DATA_DIR = "{}/.{}".format(os.environ['HOME'], PROGRAM_NAME)
-
-# Logging
-logger = logging.getLogger()
+debug = True
 
 #
 # Helper Functions
@@ -43,83 +107,23 @@ def main():
 
     init()
 
-    parser = argparse.ArgumentParser(description='Fingerprint an OFX server.')
-    parser.add_argument('ofx-url', help='URL of OFX server to test')
+    parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
+    parser.add_argument('url',
+            help='URL of OFX server to test')
+    parser.add_argument('-f', '--fid',
+            help='Financial ID of Institution',
+            required=False)
+    parser.add_argument('-o', '--org',
+            help='Organization within the Institution',
+            required=False)
     args = parser.parse_args()
 
-    # Options Parsing
-    # postern http://foo.bar/ofx.dll
-    #         -c clear cache
-    #
-    # postern
-    # Enter name of financial institution
-    # > Cit
-    # 1) Citi Bank
-    # 2) Citi Bank Financial
-    # Select a financial institution:
-    # > 2
-
-    # Output
-    # 1) Connection Methods
-    #    * DirectConnect (OFX)
-    #    * Express Web Connect (Intuit API)
-    #    * Web Connect (File download)
-    # 2) Capabilities
-    #    * Checking
-    #    * Savings
-    #    * Bill Pay
-    #    * etc...
-    # 3) Connection Info
-    #    * URL
-    #    * SSL Certificate expiration date
-    # 4) Fingerprint
-    #    * Service Provider?
-    #    * HTTP Server
-    #    * Application Framework
-    #    * OFX Server
-    #      - Company
-    #      - Product
-    #      - Version
-    # 5) Tests
-    #    * Information Disclosure
-    #    * Zombie Server
-    #    * Null values returned
-    #    * Long lived session keys
-    #    * XSS
-    #    * TLS
-
-    # Download FI list
-    # - cache it
-    # - create .postern in homedir
-
-    # Search
-    # - read FI data file
-    # - pull capabilities from FI file
+    if debug: print(args)
 
     # Send Requests
     # - cache the results
     # - determine TLS issues
 
-    # Capabilities
-    # - pull from PROFILE request
-    # - else pull from fidata
-
-    # Fingerprint
-    # - review responses
-    # - print results
-
-    # Test
-    # - review responses
-    # - print results
-
 
 if __name__ == '__main__':
-    file_handler = logging.FileHandler(os.getcwd() + os.sep + "unittest" +
-            ".log")
-    file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s [%(levelname)s][%(name)s][%(lineno)d] %(message)s'))
-    logger = logging.getLogger()
-    logger.addHandler(file_handler)
-    logger.setLevel(logging.WARNING)
-
     main()
